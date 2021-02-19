9f677734113c5a97a6f25a882c3f75da6c148ccc
junhee <wjunhee102@gmail.com>
2021-02-20 04:00:36 +0900

    Updates

--- precache-manifest.6118369302af84b5b8ad23cb47bf57cd.js
+++ precache-manifest.6118369302af84b5b8ad23cb47bf57cd.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ed824855532248f3c12c0593f44fb53c",
+    "revision": "42fe1d16dcb5955af7a68dde644c702f",
     "url": "/switching-app/index.html"
   },
   {
-    "revision": "119044cb37bd1e308c04",
+    "revision": "e04cddbbd0fc245be87d",
     "url": "/switching-app/static/css/main.2811580f.chunk.css"
   },
   {
-    "revision": "2bfb3bf9635dbfc8dd6d",
-    "url": "/switching-app/static/js/2.40fddef8.chunk.js"
+    "revision": "cb353be2cc19ed7aa133",
+    "url": "/switching-app/static/js/2.08abd30f.chunk.js"
   },
   {
     "revision": "f576af3254df0679363d3e6ffe804e36",
-    "url": "/switching-app/static/js/2.40fddef8.chunk.js.LICENSE.txt"
+    "url": "/switching-app/static/js/2.08abd30f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "119044cb37bd1e308c04",
-    "url": "/switching-app/static/js/main.69ab0be9.chunk.js"
+    "revision": "e04cddbbd0fc245be87d",
+    "url": "/switching-app/static/js/main.39fd92e1.chunk.js"
   },
   {
     "revision": "074a0e5c1fe66255ad37",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/switching-app/precache-manifest.a43b566ca62f5805f6b6540df1d5e3cb.js"
+  "/switching-app/precache-manifest.6118369302af84b5b8ad23cb47bf57cd.js"
 );
 
 self.addEventListener('message', (event) => {
